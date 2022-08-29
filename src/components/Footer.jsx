6dd0bae3f086b4b4ccbcc2ac449ae85fff214d6c
Yaroslavpnts <rofem2335@gmail.com>
2022-08-29 13:26:55 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://yaroslavpnts.github.io/react-shop/"
+            rel="noreferrer"
+            target="_blank"
+          >
             Repo
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ const Header = () => {
   return (
     <nav className="light-blue darken-1">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Repo</a>
+            <a href="https://yaroslavpnts.github.io/react-shop/" target="_blank" rel="noreferrer">
+              Repo
+            </a>
           </li>
         </ul>
       </div>
